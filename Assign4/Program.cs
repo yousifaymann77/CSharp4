19a1c35e3d0bc3a109eef574d0f39a9a90842af0
yousif <yshehab693@gmail.com>
2026-09-02 01:20:08 +0300

    Question 2

--- Assign4/Program.cs
+++ Assign4/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assign4
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assign4
 {
     internal class Program
     {
@@ -6,8 +8,15 @@
         {
             #region Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price(index 1).
             double[] prices = { 25.5, 40.0, 33.75 };
-            Console.WriteLine(prices[1]); 
+            Console.WriteLine(prices[1]);
             #endregion
+
+            #region Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies andshelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+            int[,] ShelfCopies = new int[2, 2] { { 3, 5 }, { 1, 4 } };
+            Console.WriteLine(ShelfCopies[1, 0]); 
+            #endregion
+
+
 
         }
     }
